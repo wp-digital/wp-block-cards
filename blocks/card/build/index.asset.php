13d7d5338408a2706421167a7874c477c226ad5f
Viktor Kuliebiakin <kuliebiakin@gmail.com>
2022-08-08 11:16:37 +0300

    fix editor styles

--- blocks/card/build/index.asset.php
+++ blocks/card/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => 'd6fe0af685824fb74af5');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '6f9266bab24cf01de262');
--- blocks/container/build/index.asset.php
+++ blocks/container/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '10dc1227ef5a520f72f7');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => 'f275fa40f2f3957d8f44');
